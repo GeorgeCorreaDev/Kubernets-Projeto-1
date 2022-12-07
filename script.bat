b02eb6cb98924e868d8613bdf96c5598ae9ab3b2
Mr. Cyber Developer <george.ac.dev@gmail.com>
2022-12-06 21:05:23 -0300

    projeto concluido
    testes feitos com sucesso tanto Win quanto Debian

---
+++ script.bat
@@ -0,0 +1,17 @@
+echo "criando as imagens...."
+
+docker build -t georgeacdev/projeto-backend:1.0 backend/.
+doker build -t georgeacdev/projeto-database:1.0 database/.
+
+echo "realizando push das imagens..."
+
+doker push georgeacdev/projeto-backend:1.0
+doker push georgeacdev/projeto-database:1.0
+
+echo "criando serviço no cluster kubernets."
+
+kubectl apply -f ./services.yml
+
+echo "criando deployments."
+
+kubectl apply -f ./deployment.yml
